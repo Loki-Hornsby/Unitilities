3ab0bef256182fc273a9a4b8cea05094855e81ba
Loki hornsby <lokihornsby@gmail.com>
2022-11-29 11:48:18 +0000

    Add to folder and fix

--- Unitilities/CustomUnityUtilities.cs
+++ Unitilities/CustomUnityUtilities.cs
@@ -7,7 +7,7 @@ using System;
 
 public static class Unitilities {
     public static class Angles {
-        float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
+        public static float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
             return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
         }
 
