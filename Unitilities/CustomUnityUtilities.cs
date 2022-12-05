b69292c2f5dc9b9b841160af10deeff3352408e3
Loki hornsby <lokihornsby@gmail.com>
2022-12-05 20:58:34 +0000

    Update CustomUnityUtilities.cs

--- Unitilities/CustomUnityUtilities.cs
+++ Unitilities/CustomUnityUtilities.cs
@@ -8,7 +8,7 @@ using System.Linq;
 
 public static class Unitilities {
     public static class Enums {
-        public static Enum GetRandomEnumValue(this Type t){
+        public static Enum GetRandomEnumValue(Type t){
             return Enum.GetValues(t)          // get values from Type provided
                 .OfType<Enum>()               // casts to Enum
                 .OrderBy(e => Guid.NewGuid()) // mess with order of results
