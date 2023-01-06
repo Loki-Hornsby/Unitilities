e9af09aa1d4097868c7e5a68fc8b412d3e094a43
Loki hornsby <lokihornsby@gmail.com>
2023-01-06 22:23:58 +0000

    Added minor comments

--- Unitilities/CustomUnityUtilities.cs
+++ Unitilities/CustomUnityUtilities.cs
@@ -9,11 +9,14 @@ using UnityEngine;
 using System;
 using System.Linq;
 
-// Version: 02.01.23 (1)
+// Version: 06.01.23
 
 public static class Unitilities {
     public static class Probability {
-        public static bool Check(int percentage, int range = 100){ // By default this returns a 1 in 100 chance
+        /// <summary>
+        /// Check the probability of something
+        /// </summary>
+        public static bool Check(int percentage, int range = 100){
             int var = UnityEngine.Random.Range(0, range + 1);
 
             return (var == percentage);
@@ -21,6 +24,9 @@ public static class Unitilities {
     }
 
     public static class Enums {
+        /// <summary>
+        /// Get a random enum value
+        /// </summary>
         public static Enum GetRandomEnumValue(Type t){
             return Enum.GetValues(t)          // get values from Type provided
                 .OfType<Enum>()               // casts to Enum
@@ -30,10 +36,16 @@ public static class Unitilities {
     }
 
     public static class Angles {
+        /// <summary>
+        /// Calculate the angle between 2 points
+        /// </summary>
         public static float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
             return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
         }
 
+        /// <summary>
+        /// Wrap an angle (fix an angle)
+        /// </summary>
         public static float WrapAngle(float angle){
             angle%=360;
             if(angle >180)
@@ -42,6 +54,9 @@ public static class Unitilities {
             return angle;
         }
 
+        /// <summary>
+        /// Unwrap an angle (break an angle)
+        /// </summary>
         public static float UnwrapAngle(float angle){
             if(angle >=0)
                 return angle;
@@ -53,19 +68,27 @@ public static class Unitilities {
     }
 
     public static class Distance {
+        /// <summary>
+        /// Check the distance between 2 floats
+        /// </summary>
         public static float Between2Floats(float a, float b){
             return Mathf.Abs(Mathf.Abs(a) - Mathf.Abs(b));
         }
     }
 
     public static class Trigger {
+        /// <summary>
+        /// Queries if layer is in selected Layer Mask
+        /// </summary>
         public static bool IsInLayerMask(int layer, LayerMask layermask){
-            // Queries if layer is in selected Layer Mask
             return layermask == (layermask | (1 << layer));
         }
     }
 
     public static class Maths {
+        /// <summary>
+        /// Simulate a curve
+        /// </summary>
         public static Vector3 GetCurve(Vector3 start, Vector3 end, Vector3 heightdir, Vector2 t){
             return Vector3.Lerp(
                 start,
@@ -74,6 +97,9 @@ public static class Unitilities {
             );
         }
 
+        /// <summary>
+        /// Calculate GCD
+        /// </summary>
         public static int GCD (int a, int b){
             while (a != 0 && b != 0){
                 if (a > b)
@@ -85,6 +111,9 @@ public static class Unitilities {
             return a | b;
         }
 
+        /// <summary>
+        /// Clamp a vector3
+        /// </summary>
         public static Vector3 ClampVector3(Vector3 value, Vector3 min, Vector3 max){
             value.x = Mathf.Clamp(value.x, min.x, max.x);
             value.y = Mathf.Clamp(value.y, min.y, max.y);
@@ -93,6 +122,9 @@ public static class Unitilities {
             return value;
         }
 
+        /// <summary>
+        /// Clamp a vector2
+        /// </summary>
         public static Vector2 ClampVector2(Vector2 value, Vector2 min, Vector2 max){
             value.x = Mathf.Clamp(value.x, min.x, max.x);
             value.y = Mathf.Clamp(value.y, min.y, max.y);
@@ -100,6 +132,9 @@ public static class Unitilities {
             return value;
         }
 
+        /// <summary>
+        /// Calculate sin or cos (similar to Mathf.PingPong but with more control)
+        /// </summary>
         public static float CalculateSinOrCos(float speed, float amplitude, bool cos){
             float val = 0f;
             float p = Time.fixedTime * Mathf.PI * speed;
